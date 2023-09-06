3208845c92bb16dcf01bd6db99d5de8d880ca604
oscar <u3572091@connect.hku.hk>
2023-09-06 17:59:39 +0800

    hi

--- dist/index.js
+++ dist/index.js
@@ -9816,7 +9816,7 @@ try {
     console.log(`Hello ${ nameToGreet }`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    const payload = JSON.stringify(github.context.payload, undefiend, 2);
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${ payload }`);
 } catch(error) {
     core.setFailed(error.message);
